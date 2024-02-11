037c06ad428b1157b7dc6b1cce387504d6f748be
Dhyaneshvar <dhyaneshcodespace@gmail.com>
2024-02-11 20:00:33 +0530

    few changes

--- Restaurant_management_system/urls.py
+++ Restaurant_management_system/urls.py
@@ -26,4 +26,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
